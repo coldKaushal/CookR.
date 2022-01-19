b3dc5084d625232c71a32accbf4f84275c08d706
coldkaushal <kauagg111@gmail.com>
2022-01-19 20:47:41 +0530

    add route and signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 //import logo from './logo.svg';
 // import './App.css';
 import React from "react";
-import Header from "./components/dashboard/header";
-import Hero from "./components/dashboard/hero";
-import Trending from "./components/dashboard/trending";
-import Footer from "./components/footer";
-import BackToTop from "./components/dashboard/back-to-top";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashBoard from "./components/dashboard/dashboard"
+import Signup from "./components/signup/signup";
 //TODO: Fix BackToTop button
 
 function App() {
-  return <div>
+  return (<BrowserRouter>
+  <Routes>
+    <Route path="/" element={<DashBoard/>}>
 
-    <Header />
-    <Hero />
-    <Trending />
-    <Footer />
-    <BackToTop />    
-  </div>
+    </Route>
+    <Route path="/signup" element={<Signup/>}>
+
+    </Route>
+  </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/dashboard/dashboard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./header";
+import Hero from "./hero";
+import Trending from "./trending";
+import Footer from "../footer";
+import BackToTop from "./back-to-top";
+
+//TODO: Fix BackToTop button
+
+function Dashboard() {
+  return <div>
+    <Header />
+    <Hero />
+    <Trending />
+    <Footer />
+    <BackToTop />    
+  </div>
+}
+
+export default Dashboard;
