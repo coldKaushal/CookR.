0e8b58e0ef31e7d16c69e2e28c60e9dc319871e4
coldkaushal <kauagg111@gmail.com>
2022-01-19 13:39:29 +0530

    set up dasboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import Header from "./components/dashboard/header";
+import Hero from "./components/dashboard/hero";
+import Trending from "./components/dashboard/trending";
+import Footer from "./components/footer";
+import BackToTop from "./components/dashboard/back-to-top";
+
+//TODO: Fix BackToTop button
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+
+    <Header />
+    <Hero />
+    <Trending />
+    <Footer />
+    <BackToTop />    
+  </div>
 }
 
 export default App;
---
+++ src/components/dashboard/hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Hero(){
+    return <section id="hero" className="d-flex align-items-center">
+
+    <div className="container" data-aos="zoom-out" data-aos-delay="100">
+      <div className="row">
+        <div className="col-xl-6">
+          <h1>Find recipes for the ingedrients you aldready have </h1>
+          <h2>We suggest you the best dishes with easiest recipies that can be made with the available ingedrients in your kitchen.</h2>
+          <a href="search.html" className="btn-search scrollto"><i className="fas fa-search"></i>  Search</a>
+          <a href="Explore.html" className="btn-explore scrollto"><i className="fas fa-dharmachakra"></i>  Explore</a>
+        </div>
+      </div>
+    </div>
+
+  </section>
+}
+
+export default Hero;
