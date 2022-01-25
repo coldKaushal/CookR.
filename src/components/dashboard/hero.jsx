cdcce53d8f772d34d3645ebb8d5044631477e0b6
coldkaushal <kauagg111@gmail.com>
2022-01-25 13:49:45 +0530

    fix search-explore bug

--- src/components/dashboard/hero.jsx
+++ src/components/dashboard/hero.jsx
@@ -10,8 +10,8 @@ function Hero(){
         <div className="col-xl-6">
           <h1>Find recipes for the ingedrients you aldready have </h1>
           <h2>We suggest you the best dishes with easiest recipies that can be made with the available ingedrients in your kitchen.</h2>
-          <a onClick={()=>navigate("./explore")} className="btn-search scrollto"><i className="fas fa-search"></i>  Search</a>
-          <a onClick={()=>navigate("./search")} className="btn-explore scrollto"><i className="fas fa-dharmachakra"></i>  Explore</a>
+          <a onClick={()=>navigate("./search")} className="btn-search scrollto"><i className="fas fa-search"></i>  Search</a>
+          <a onClick={()=>navigate("./explore")} className="btn-explore scrollto"><i className="fas fa-dharmachakra"></i>  Explore</a>
         </div>
       </div>
     </div>
